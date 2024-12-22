b042c614b69674f654a6be695bef0a5527b0e17c
Gamingninduwau <Induwaramax78@gmail.com>
2024-12-22 20:54:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "mmRBFR6Y#LzFleTg8qeBeV4aQQwMwT_SWS6DtruUoREAUbKbgPQ0", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+MODE: process.env.MODE || "praivate",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 AUTO_REACT: process.env.AUTO_REACT || "false",
